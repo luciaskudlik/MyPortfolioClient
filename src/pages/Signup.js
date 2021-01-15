b06903f1fd622bccfa93a7e9543020c6c26e2491
Lucia Skudlik <lucia.skudlik@gmail.com>
2021-01-15 19:25:36 +0100

    update signup functionality

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,39 +1,64 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { withAuth } from './../context/auth-context';
+import { withAuth } from "./../context/auth-context";
 
 class Signup extends Component {
-  state = { username: "", password: "" };
+  state = { username: "", occupation: "", email: "", password: "" };
 
-  handleFormSubmit = event => {
+  handleFormSubmit = (event) => {
     event.preventDefault();
-    const { username, password } = this.state;
-    
-    this.props.signup( username, password );
+    const { username, occupation, email, password } = this.state;
+
+    this.props.signup(username, occupation, email, password);
   };
 
-  handleChange = event => {
+  handleChange = (event) => {
     const { name, value } = event.target;
     this.setState({ [name]: value });
   };
 
   render() {
-    const { username, password } = this.state;
+    const { username, occupation, email, password } = this.state;
     return (
       <div>
         <h1>Sign Up</h1>
 
         <form onSubmit={this.handleFormSubmit}>
-
           <label>Username:</label>
-          <input type="text" name="username" value={username} onChange={this.handleChange} />
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={this.handleChange}
+          />
+
+          <label>Occupation:</label>
+          <input
+            type="text"
+            name="occupation"
+            value={occupation}
+            onChange={this.handleChange}
+          />
+
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+          />
 
           <label>Password:</label>
-          <input type="password" name="password" value={password} onChange={this.handleChange} />
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+          />
 
           <input type="submit" value="Signup" />
         </form>
-        
+
         <p>Already have account?</p>
         <Link to={"/login"}> Login</Link>
       </div>
@@ -41,10 +66,7 @@ class Signup extends Component {
   }
 }
 
-
-
 export default withAuth(Signup);
-
 
 // const EnhancedSignup = withAuth(Signup)
 // export default EnhancedSignup;
